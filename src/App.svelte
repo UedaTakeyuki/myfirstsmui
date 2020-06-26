f1497eb36089b165a5d6d57514b9db5736faf0ee
Takeyuki UEDA <ueda@latelierdueda.com>
2020-06-26 19:17:33 +0900

    fix

--- src/App.svelte
+++ src/App.svelte
@@ -101,6 +101,41 @@ let menu;
     </ActionIcons>
   </Actions>
 </Card>
+
+<Card style="width: 360px;">
+  <PrimaryAction on:click={() => doAction('openItemPage')}>
+    <Media style="background-image: url(https://via.placeholder.com/320x180.png?text=16x9);" aspectRatio="16x9">
+      <MediaContent>
+        <div style="color: #fff; position: absolute; bottom: 16px; left: 16px;">
+          <h2 class="mdc-typography--headline6" style="margin: 0;">Glorious Item</h2>
+          <h3 class="mdc-typography--subtitle2" style="margin: 0;">People are saying you need to buy it.</h3>
+        </div>
+      </MediaContent>
+    </Media>
+    <Content class="mdc-typography--body2">
+      This item is so glorious that you would have to be some kind of fool to not immediately buy it.
+    </Content>
+  </PrimaryAction>
+  <Actions>
+    <ActionButtons>
+      <Button on:click={() => doAction('addToCart')}>
+        <Label>Add to Card</Label>
+      </Button>
+      <Button on:click={() => doAction('buyNow')}>
+        <Label>Buy Now</Label>
+      </Button>
+    </ActionButtons>
+    <ActionIcons>
+      <IconButton on:click={() => doAction('addToFavoritesToggle')} toggle aria-label="Add to favorites" title="Add to favorites">
+        <Icon class="material-icons" on>favorite</Icon>
+        <Icon class="material-icons">favorite_border</Icon>
+      </IconButton>
+      <IconButton class="material-icons" on:click={() => doAction('share')} title="Share">share</IconButton>
+    </ActionIcons>
+  </Actions>
+</Card>
+
+
 </main>
 
 <style>
@@ -109,7 +144,7 @@ let menu;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
+	}	
 
 	h1 {
 		color: #ff3e00;
@@ -123,4 +158,5 @@ let menu;
 			max-width: none;
 		}
 	}
+
 </style>
