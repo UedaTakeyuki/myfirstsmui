0f67be3e6b75394a90d07a92445412d273caf1d9
Takeyuki UEDA <ueda@latelierdueda.com>
2021-06-11 20:22:45 +0900

    updated but not good to app bar

--- src/App.svelte
+++ src/App.svelte
@@ -6,135 +6,39 @@
   import Textfield from '@smui/textfield';
   import HelperText from '@smui/textfield/helper-text';
 	import {Label, Icon} from '@smui/common';
-  let superText = '死ねハゲ！';
 	
 	import Card, {Content, PrimaryAction, Media, MediaContent, Actions, ActionButtons, ActionIcons} from '@smui/card';
 	import IconButton from '@smui/icon-button';
 	import List, {Item, Separator, Text} from '@smui/list';
-	function doAction(action) {
-    alert('You did an action: '+action);
-	};
 
-import TopAppBar, {Section, Title, Row} from '@smui/top-app-bar';
-import Menu from '@smui/menu';
-import {GroupItem} from '@smui/button';
-let dense = true;
-let menu;
+  import TopAppBar, {Section, Title, Row} from '@smui/top-app-bar';
+  import Menu from '@smui/menu';
+  import {GroupItem} from '@smui/button';
+  let dense = true;
+  let menu;
+  let topAppBar;
+  let title = "禿！";
 </script>
 
 <main>
-<TopAppBar variant="static" {dense}>
-  <div class="top-bar-container">
+  <TopAppBar bind:this={topAppBar} {dense}>
     <Row>
       <Section>
-        <IconButton>
-          <a href="#/"><img src="../dist/images/logo_white_fat.svg" alt="montage"></a>
-        </IconButton>
-        <Title>Montage Admin</Title>
+        <IconButton class="material-icons">menu</IconButton>
+        <Title>{title}</Title>
       </Section>
       <Section align="end" toolbar>
-        <div use:GroupItem>
-          <Button color="secondary" on:click={() => menu.setOpen(true)}>
-            <Icon class="material-icons" aria-label="Slaporte">person</Icon>
-            <Label>Slaporte</Label>
-          </Button>
-          <Menu bind:this={menu} anchorCorner="BOTTOM_LEFT">
-            <List>
-                <Item><Text>Settings</Text></Item>
-                <Item><Text>Log out</Text></Item>
-            </List>
-          </Menu>
-        </div>
+        <IconButton class="material-icons" aria-label="Download"
+          >file_download</IconButton
+        >
       </Section>
     </Row>
-  </div>
-</TopAppBar>
+  </TopAppBar>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 
-<Button on:click={() => alert('Clicked!')}>Just a Button</Button>
-<Button variant="raised"><Label>Raised Button, Using a Label</Label></Button>
-<Button some-arbitrary-prop="placed on the actual button">Button</Button>
-
-<Fab on:click={() => alert('Clicked!')} extended>
-  <Icon class="material-icons" style="margin-right: 12px;">favorite</Icon>
-  <Label>Extended FAB</Label>
-</Fab>
-
-<Textfield
-  bind:value={superText}
-  label="Super Text"
-  input$aria-controls="super-helper"
-  input$aria-describedby="super-helper"
-/>
-<HelperText id="super-helper">What you put in this box will become super!</HelperText>
-
-<Card style="width: 360px;">
-  <PrimaryAction on:click={() => doAction('openItemPage')}>
-    <Media style="background-image: url(https://via.placeholder.com/320x180.png?text=16x9);" aspectRatio="16x9">
-      <MediaContent>
-        <div style="color: #fff; position: absolute; bottom: 16px; left: 16px;">
-          <h2 class="mdc-typography--headline6" style="margin: 0;">Glorious Item</h2>
-          <h3 class="mdc-typography--subtitle2" style="margin: 0;">People are saying you need to buy it.</h3>
-        </div>
-      </MediaContent>
-    </Media>
-    <Content class="mdc-typography--body2">
-      This item is so glorious that you would have to be some kind of fool to not immediately buy it.
-    </Content>
-  </PrimaryAction>
-  <Actions>
-    <ActionButtons>
-      <Button on:click={() => doAction('addToCart')}>
-        <Label>Add to Card</Label>
-      </Button>
-      <Button on:click={() => doAction('buyNow')}>
-        <Label>Buy Now</Label>
-      </Button>
-    </ActionButtons>
-    <ActionIcons>
-      <IconButton on:click={() => doAction('addToFavoritesToggle')} toggle aria-label="Add to favorites" title="Add to favorites">
-        <Icon class="material-icons" on>favorite</Icon>
-        <Icon class="material-icons">favorite_border</Icon>
-      </IconButton>
-      <IconButton class="material-icons" on:click={() => doAction('share')} title="Share">share</IconButton>
-    </ActionIcons>
-  </Actions>
-</Card>
-
-<Card style="width: 360px;">
-  <PrimaryAction on:click={() => doAction('openItemPage')}>
-    <Media style="background-image: url(https://via.placeholder.com/320x180.png?text=16x9);" aspectRatio="16x9">
-      <MediaContent>
-        <div style="color: #fff; position: absolute; bottom: 16px; left: 16px;">
-          <h2 class="mdc-typography--headline6" style="margin: 0;">Glorious Item</h2>
-          <h3 class="mdc-typography--subtitle2" style="margin: 0;">People are saying you need to buy it.</h3>
-        </div>
-      </MediaContent>
-    </Media>
-    <Content class="mdc-typography--body2">
-      This item is so glorious that you would have to be some kind of fool to not immediately buy it.
-    </Content>
-  </PrimaryAction>
-  <Actions>
-    <ActionButtons>
-      <Button on:click={() => doAction('addToCart')}>
-        <Label>Add to Card</Label>
-      </Button>
-      <Button on:click={() => doAction('buyNow')}>
-        <Label>Buy Now</Label>
-      </Button>
-    </ActionButtons>
-    <ActionIcons>
-      <IconButton on:click={() => doAction('addToFavoritesToggle')} toggle aria-label="Add to favorites" title="Add to favorites">
-        <Icon class="material-icons" on>favorite</Icon>
-        <Icon class="material-icons">favorite_border</Icon>
-      </IconButton>
-      <IconButton class="material-icons" on:click={() => doAction('share')} title="Share">share</IconButton>
-    </ActionIcons>
-  </Actions>
-</Card>
-
+  <Button on:click={() => alert('Clicked!')}>Just a Button</Button>
+  <Button variant="raised"><Label>Raised Button, Using a Label</Label></Button>
+  <Button some-arbitrary-prop="placed on the actual button">Button</Button>
 
 </main>
 
